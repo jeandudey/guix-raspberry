a2bf3620e8fee94f8c3b8a1ee9267a3d984e7342
Jean-Pierre De Jesus DIAZ <me@jeandudey.tech>
2025-02-27 18:40:31 +0100

    Update edk2-rpi4.
    Use the GNU Guix as the logo.

--- raspberry/packages/firmware.scm
+++ raspberry/packages/firmware.scm
@@ -2,13 +2,17 @@
 ;;; SPDX-License-Identifier: GPL-3.0-or-later
 
 (define-module (raspberry packages firmware)
+  #:use-module (gnu artwork)
   #:use-module (gnu packages admin)
   #:use-module (gnu packages assembly)
   #:use-module (gnu packages cross-base)
   #:use-module (gnu packages firmware)
+  #:use-module (gnu packages imagemagick)
+  #:use-module (gnu packages inkscape)
   #:use-module (gnu packages linux)
   #:use-module (gnu packages perl)
   #:use-module (gnu packages python)
+  #:use-module (guix build-system trivial)
   #:use-module (guix gexp)
   #:use-module (guix git-download)
   #:use-module ((guix licenses) #:prefix license:)
@@ -31,13 +35,110 @@
                           "SMC_PCI_SUPPORT=1")
                     #$make-flags)))))))
 
+
+;; The EDK2 source package necessary to build the image.
+;;
+;; Only uses the Raspberry Pi platform code and removes the
+;; TrustedFirmware binary blob and replaces it with our build
+;; and also we replace the Raspberry Pi logo as it is trademarked.
+;;
+;; While the trademark rules in the Raspberry Pi website are very clear
+;; and this package wouldn't technically be in breach it is better
+;; just to replace it with another to avoid any issues altogether.
+;;
+;; Better be safe than sorry.
+(define-public edk2-rpi4-osi
+  (let ((revision "0")
+        (commit "ea2040c2d4e2200557e87b9f9fbd4f8fb7a2b6e8"))
+    (package
+      (name "edk2-rpi4-osi")
+      (version (git-version "0.0.0" revision commit))
+      (source (origin
+                (method git-fetch)
+                (uri (git-reference
+                       (url "https://github.com/tianocore/edk2-non-osi")
+                       (commit commit)))
+                (file-name (git-file-name "edk2-non-osi" version))
+                (modules '((guix build utils)))
+                (snippet
+                  #~(for-each delete-file-recursively
+                              '("Drivers"
+                                "Emulator"
+                                "Platform/AMD"
+                                "Platform/ARM"
+                                "Platform/Hisilicon"
+                                "Platform/Intel"
+                                "Platform/LeMaker"
+                                "Platform/Qemu/Sbsa"
+                                "Platform/RaspberryPi/Drivers/LogoDxe/License.txt"
+                                "Platform/RaspberryPi/Drivers/LogoDxe/Logo.bmp"
+                                "Platform/RaspberryPi/Drivers/LogoDxe/Logo.eps"
+                                "Platform/RaspberryPi/RPi3"
+                                "Platform/RaspberryPi/RPi4"
+                                "Platform/Socionext"
+                                "Platform/SoftIron"
+                                "Silicon")))
+                (sha256
+                 (base32
+                  "0yw18raqhyin5rak9maa5znwqnyzaqqsp22g78865xmj06akh1l1"))))
+      (build-system trivial-build-system)
+      (arguments
+        (list #:modules '((guix build utils))
+              #:builder
+              #~(begin
+                  (use-modules (guix build utils))
+
+                  (let* ((dxe-dir "Platform/RaspberryPi/Drivers/LogoDxe")
+                         (in-dxe-dir (string-append (assoc-ref %build-inputs "source")
+                                                    "/" dxe-dir))
+                         (out-dxe-dir (string-append #$output
+                                                     "/src/edk2-rpi4-osi/"
+                                                     dxe-dir)))
+                    (mkdir-p out-dxe-dir)
+                    (copy-recursively in-dxe-dir out-dxe-dir)
+
+                    (mkdir-p (string-append
+                               #$output "/src/edk2-rpi4-osi/Platform"
+                               "/RaspberryPi/RPi4/TrustedFirmware"))
+                    (copy-file #$(file-append
+                                   (this-package-input "arm-trusted-firmware-rpi4")
+                                   "/bl31.bin")
+                               (string-append
+                                 #$output "/src/edk2-rpi4-osi/Platform"
+                                 "/RaspberryPi/RPi4/TrustedFirmware/bl31.bin"))
+
+                    ;; Convert SVG to PNG.
+                    (invoke #$(file-append
+                                (this-package-native-input "inkscape")
+                                "/bin/inkscape")
+                            #$(file-append %artwork-repository
+                                           "/logo/Guix.svg")
+                            "--export-background=black"
+                            "-o" (string-append out-dxe-dir "/Logo.png"))
+
+                    ;; Convert to BMP.
+                    (invoke #$(file-append
+                                (this-package-native-input "imagemagick")
+                                "/bin/convert")
+                            (string-append out-dxe-dir "/Logo.png")
+                            (string-append "BMP3:" out-dxe-dir "/Logo.bmp"))))))
+      (native-inputs (list imagemagick inkscape))
+      (inputs (list arm-trusted-firmware-rpi4))
+      (home-page "https://github.com/tianocore/edk2-non-osi")
+      (synopsis "Logo driver sources for Raspberry Pi 4 EDK2")
+      (description "This package provides modified sources for displaying a
+logo in the Raspberry Pi 4 EDK2 UEFI firmware.  It is based on the non-OSI
+sources of EDK2 but removing all of the binary blobs and trademarked logos
+so that it is safe for usage and redistribution.")
+      (license license:bsd-2))))
+
 (define-public edk2-rpi4
   (let ((toolchain "GCC")
         (arch-string "AARCH64"))
     (package
       (inherit edk2-tools)
       (name "edk2-rpi4")
-      (version "202408")
+      (version "202502")
       (source (origin
                 (method git-fetch)
                 (uri (git-reference
@@ -48,7 +149,7 @@
                 (file-name (git-file-name "edk2-tools" version))
                 (sha256
                  (base32
-                  "0806470j5rdn2l9mngc5yriy0gas3017gx74frqkwr40m175m1ys"))))
+                  "026h7hadzj1zflgf4qzdby3gmgcqh3m5rvn2yr92jjwn4z8c51la"))))
       (arguments
        (list
         #:tests? #f                     ; No check target.
@@ -82,7 +183,8 @@
                           (string-append
                             cwd ":"
                             #$(this-package-native-input "edk2-platforms") ":"
-                            #$(this-package-native-input "edk2-non-osi")))
+                            #$(this-package-native-input "edk2-rpi4-osi")
+                            "/src/edk2-rpi4-osi"))
                   (invoke "bash" "edksetup.sh")
                   (substitute* "Conf/target.txt"
                     (("^TARGET[ ]*=.*$") "TARGET = RELEASE\n")
@@ -128,7 +230,7 @@
          ("util-linux:lib" ,util-linux "lib")
          ("edk2-platforms"
           ,(let ((revision "0")
-                 (commit "6146fd7abcf6bfa083d33d1dfdc750694fc040a7"))
+                 (commit "728c8bb974be69b4034fad7a1c60917cca2dd03d"))
              (origin
                (method git-fetch)
                (uri (git-reference
@@ -139,27 +241,8 @@
                                (git-version "0.0.0" revision commit)))
                (sha256
                 (base32
-                 "14dq3j3bpg4s5vjxszz2nhdfqc0094fxan3amd9ssayb8v7rdg98")))))
-         ("edk2-non-osi"
-          ,(let ((revision "0")
-                 (commit "0544808c623bb73252310b1e5ef887caaf08c34b"))
-             (origin
-               (method git-fetch)
-               (uri (git-reference
-                      (url "https://github.com/tianocore/edk2-non-osi")
-                      (commit commit)))
-               (file-name
-                (git-file-name "edk2-non-osi"
-                               (git-version "0.0.0" revision commit)))
-               (modules '((guix build utils)))
-               (snippet
-                #~(begin
-                    (for-each delete-file (find-files "." "\\.(exe|bin)$"))
-                    (copy-file #$(file-append arm-trusted-firmware-rpi4 "/bl31.bin")
-                               "Platform/RaspberryPi/RPi4/TrustedFirmware/bl31.bin")))
-               (sha256
-                (base32
-                 "037py14vfdxf8cv16fmim8s1c0fyagwysw07qv5slks7pjkzbl6k")))))
+                 "1z1kdl6lsfrj151pnhqrfzs8xfwcgysyfq23kc5d93cc0vr2znm0")))))
+         ("edk2-rpi4-osi" ,edk2-rpi4-osi)
          ,@(if (not (string-prefix? "aarch64" (%current-system)))
                `(("cross-gcc" ,(cross-gcc "aarch64-linux-gnu"))
                  ("cross-binutils" ,(cross-binutils "aarch64-linux-gnu")))
